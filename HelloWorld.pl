b9193456fb6dac8e2b9b4454c1290086537f20cc
kgbercan <kgbercan@gmail.com>
2016-02-17 08:55:53 -0500

    playing with infinite loops in foreach loops

--- HelloWorld.pl
+++ HelloWorld.pl
@@ -2,3 +2,14 @@
 # This is Karina's first program in Perl.
 
 print "Hello, World!\n";
+
+@ary = qw(cat hat bat sat rat mat pat gnat);
+
+$, = "\n";
+
+print(@ary);
+
+foreach $x (@ary){
+	print $x;
+#	push (@ary,$x); this creates an infinite loop
+}
